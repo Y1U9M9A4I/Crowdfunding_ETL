735b6847423116b308ee45644e2d9207de88c1da
Y1U9M9A4I <situ.ym@gmail.com>
2024-05-27 18:29:38 -0400

    sql all functioning

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -7,30 +7,30 @@ drop table if exists "Contacts";
 drop table if exists "Campaign";
 
 CREATE TABLE "Category" (
-    "category_id" varchar(10) NOT NULL,
-    "category" varchar(15) NOT NULL,
+    "category_id" varchar(150) NOT NULL,
+    "category" varchar(150) NOT NULL,
     PRIMARY KEY ("category_id")
 );
 
 CREATE TABLE "Subcategory" (
-    "subcategory_id" varchar(15) NOT NULL,
-    "subcategory" varchar(15) NOT NULL,
+    "subcategory_id" varchar(150) NOT NULL,
+    "subcategory" varchar(150) NOT NULL,
     PRIMARY KEY ("subcategory_id")
 );
 
 CREATE TABLE "Contacts" (
     "contact_id" int NOT NULL,
-    "first_name" varchar(15) NOT NULL,
-    "last_name" varchar(15) NOT NULL,
-    "email" varchar(100) NOT NULL,
+    "first_name" varchar(150) NOT NULL,
+    "last_name" varchar(150) NOT NULL,
+    "email" varchar(150) NOT NULL,
     PRIMARY KEY ("contact_id")
 );
 
 CREATE TABLE "Campaign" (
     "cf_id" int NOT NULL,
     "contact_id" int NOT NULL,
-    "company_name" varchar(100) NOT NULL,
-    "description" varchar(100) NOT NULL,
+    "company_name" varchar(150) NOT NULL,
+    "description" varchar(150) NOT NULL,
     "goal" decimal NOT NULL,
     "pledged" decimal NOT NULL,
     "backers_count" int NOT NULL,
@@ -38,10 +38,15 @@ CREATE TABLE "Campaign" (
     "currency" varchar(5) NOT NULL,
     "launch_date" date NOT NULL,
     "end_date" date NOT NULL,
-    "category_id" varchar(10) NOT NULL,
-    "subcategory_id" varchar(15) NOT NULL,
+    "category_id" varchar(150) NOT NULL,
+    "subcategory_id" varchar(150) NOT NULL,
     PRIMARY KEY ("cf_id"),
 	FOREIGN KEY ("category_id") REFERENCES "Category"("category_id"),
 	FOREIGN KEY ("subcategory_id") REFERENCES "Subcategory"("subcategory_id"),
 	FOREIGN KEY ("contact_id") REFERENCES "Contacts"("contact_id")
 );
+
+SELECT * FROM "Category";
+SELECT * FROM "Subcategory";
+SELECT * FROM "Contacts";
+SELECT * FROM "Campaign";
